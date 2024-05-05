53a5619a4306799c25e9d32341a7c9765fb83e11
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-05 21:15:18 +0300

    Update ECommerceApiApplication.java

--- src/main/java/com/ceksioglu/ecommerce_api/ECommerceApiApplication.java
+++ src/main/java/com/ceksioglu/ecommerce_api/ECommerceApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceApiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApiApplication.class, args);
 	}
