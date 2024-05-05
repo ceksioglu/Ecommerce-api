61c322b8cd401672ec849ba8bee76e4245b33f17
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-05-05 22:55:58 +0300

    Configuration complete.
    All bugs fixed, first successful build.

--- src/main/java/com/github/ceksioglu/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/github/ceksioglu/ecommerceapi/EcommerceApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApiApplication {
+public class ECommerceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApiApplication.class, args);
+		SpringApplication.run(ECommerceApiApplication.class, args);
 	}
 
 }
